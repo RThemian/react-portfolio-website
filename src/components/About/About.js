37f9062053dc666a33855113807a72b9f94100a8
RThemian <62709684+RThemian@users.noreply.github.com>
2022-04-28 07:27:02 -0600

    adding components
    Home, About Me, adding profile png

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,7 +1,22 @@
+import classes from './About.module.css'
+import PageHeader from '../PageHeader/PageHeader';
+
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
+        <div className={classes.AboutMe}>
+            <PageHeader title = {'About Me'} />
+            <div className={classes.Container}>
+                <div className={classes.Text}>
+                    <h2>Hello there! I'm Tomas</h2>
+                    <p>
+                        Software developer based in Fort Collins, Colorado, USA. 
+                        Computer science and math teacher for 11 years.
+                    </p>
+                    <div className={classes.Photo}>
+                        <img className={classes.Me} src ={} alt = 'me'></img>
+                    </div>
+                </div>
+            </div>
         </div>
       );
 }

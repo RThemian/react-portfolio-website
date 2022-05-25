19b271bca5c48f01a647f1899d980a372b893119
RThemian <62709684+RThemian@users.noreply.github.com>
2022-05-25 07:12:52 -0600

    adding contact form
    not formatted well

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,43 @@
+import React from 'react';
+
 const Contact = () => {
     return ( 
-        <div id="contact">
+    <div id="contact" className="container">
             <h1>Contact</h1>
-        </div>
+        <form>
+            <input 
+            type = 'text' 
+            name = 'name' 
+            placeholder="Name" 
+            />
+
+            <input 
+            type = 'email'
+            name = 'email'
+            placeholder="Email"
+            />
+
+            <input 
+            type = 'subject'
+            name = 'subject'
+            placeholder="Subject"
+            />
+
+           <textarea 
+           name = 'message' 
+           cols = '30'
+           rows = '10'
+           placeholder="Message"
+           />
+
+           <input 
+           type = 'file'
+           name = 'resume[]'
+           />
+           <button type = 'submit'>Submit</button>
+    </form>
+
+</div>
      );
 }
  

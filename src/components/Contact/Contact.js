6e187b49bc1f9be3ecf0a08226c71fab9afa49c4
RThemian <62709684+RThemian@users.noreply.github.com>
2022-05-25 21:27:17 -0600

    email form ready?
    not emailing me

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,29 +1,44 @@
 import React from 'react';
+import classes from './Contact.module.css'
+
+
 
 const Contact = () => {
     return ( 
-    <div id="contact" className="container">
+    <div id="contact" className={classes.Container}>
             <h1>Contact</h1>
-        <form>
+        <form method="POST"
+        action = 'https://getform.io/f/08deec31-286d-4c71-8052-f78cec986218'>
+            <div>
             <input 
+        
             type = 'text' 
             name = 'name' 
             placeholder="Name" 
             />
+            </div>
 
+            <div>
             <input 
+            
             type = 'email'
             name = 'email'
             placeholder="Email"
             />
+            </div>
 
+            <div>
             <input 
+         
             type = 'subject'
             name = 'subject'
             placeholder="Subject"
             />
 
+            </div>
+
            <textarea 
+     
            name = 'message' 
            cols = '30'
            rows = '10'
@@ -31,10 +46,13 @@ const Contact = () => {
            />
 
            <input 
+           className='container'
            type = 'file'
            name = 'resume[]'
            />
+           
            <button type = 'submit'>Submit</button>
+           
     </form>
 
 </div>

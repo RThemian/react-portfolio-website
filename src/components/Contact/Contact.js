8851a77718b7aa1a6799cc9e76c7cc79a96c4333
RThemian <62709684+RThemian@users.noreply.github.com>
2022-05-27 14:34:39 -0600

    updated message box for contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const Contact = () => {
     return ( 
         <>
@@ -16,7 +18,7 @@ const Contact = () => {
       </label>
       <input 
       name = 'name' placeholder="name"
-      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text"/>
+      className="hover:border-black shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text"/>
     </div>
     
     
@@ -26,15 +28,16 @@ const Contact = () => {
         Email
       </label>
       <input 
+      
       name = 'email' placeholder="email"
-      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="email"/>
+      className="hover:border-black shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="email"/>
     </div>
     <div className="mb-6">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
-        Password
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="message">
+        Message
       </label>
-      <input className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
-      <p className="text-red-500 text-xs italic">Please choose a password.</p>
+      <textarea  className="resize shadow appearance-none border  hover:border-black rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************"/>
+      <p className="text-xs italic">Please write your message.</p>
     </div>
     <div className="flex items-center justify-between">
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">

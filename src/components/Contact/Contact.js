0d7d7938fc13c9fc7028c339236710c5988e5501
RThemian <62709684+RThemian@users.noreply.github.com>
2022-05-26 22:02:28 -0600

    updated name, email, and password

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,12 +6,28 @@ const Contact = () => {
         </div>
         
         <div className="w-full max-w-xs">
-  <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+  <form 
+  action ='https://getform.io/f/08deec31-286d-4c71-8052-f78cec986218'
+  method="POST" className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    
     <div className="mb-4">
-      <label className="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="name">
+        Full name
       </label>
-      <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+      <input 
+      name = 'name' placeholder="name"
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text"/>
+    </div>
+    
+    
+    
+    <div className="mb-4">
+      <label className="block text-gray-700 text-sm font-bold mb-2" for="email">
+        Email
+      </label>
+      <input 
+      name = 'email' placeholder="email"
+      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="email"/>
     </div>
     <div className="mb-6">
       <label className="block text-gray-700 text-sm font-bold mb-2" for="password">
@@ -22,10 +38,10 @@ const Contact = () => {
     </div>
     <div className="flex items-center justify-between">
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-        Sign In
+        Submit
       </button>
       <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
-        Forgot Password?
+        Add file
       </a>
     </div>
   </form>

0be8b356563c9507cb99a67fd5679d7326a7fa8f
RThemian <62709684+RThemian@users.noreply.github.com>
2022-06-19 09:21:52 -0600

    update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,11 +2,42 @@ import classes from './About.module.css'
 import PageHeader from '../PageHeader/PageHeader';
 import profileMe from './../../images/profileMe.PNG'
 import gitHubIcon from './../../images/gitHubIcon.svg'
+import React from 'react';
+import { MDBRipple } from 'mdb-react-ui-kit';
+
 
 
 const About = () => {
     return (
+    
+
+<div className="container-fluid">
+  <h3 className="text-primary text-center">jQuery Playground</h3>
+  <div className="row">
+    <div className="col-xs-6">
+      <div className="well">
+
+      <a href="https://github.com/RThemian" target="_blank" rel="noreferrer"> 
+                            <img className = "img-thumbnail" src ={gitHubIcon} alt = 'gitHub'></img>
+                             </a>
+
+      </div>
+    </div>
+    <div className="col-xs-6">
+      <div >
+
+      <a href="https://github.com/RThemian" target="_blank" rel="noreferrer"> 
+                            <img className = "img-thumbnail" src ={gitHubIcon} alt = 'gitHub'></img>
+                             </a>
+
+      </div>
+    </div>
+  </div>
+
+
         <div className={classes.AboutMe} id="about">
+             
+        
             <PageHeader title = {'About Me'} />
             <div className={classes.Container}>
                 <div className={classes.Text}>
@@ -16,25 +47,46 @@ const About = () => {
                         Computer science, math, and Spanish teacher for 11 years.
                         I problem solve in ReactJS
                     </p>
-                    
+                 
 
                     
-                    <div className={classes.Photo}>
-                        <img className={classes.Me} src ={profileMe} alt = 'me'></img>
-                    </div>
-                    <label id ="gitHub">Find me on GitHub</label>
-                    <div className={classes.Photo}>
+                    <div className='row'>
+                         
+                         
+                        <div className = 'col-xs-6'></div>
+                            <div className = "well">
+                        <label id ="gitHub">Find me on GitHub</label>
                         
-                        <a href="https://github.com/RThemian" target="_blank" rel="noreferrer">
-                            <img className={classes.Me} src ={gitHubIcon} alt = 'gitHub'></img>
-                        </a>
-                    </div>
-                    
-       
-       
+                            <a href="https://github.com/RThemian" target="_blank" rel="noreferrer"> 
+                            <img className = "img-thumbnail" src ={gitHubIcon} alt = 'gitHub'></img>
+                             </a>
+                        
+                        </div>
+                        </div>
+
+                        <div className = 'col-xs-6'></div>
+                            <div className = "well">
+                        <label id ="gitHub">Find me on GitHub</label>
+                        
+                            <a href="https://github.com/RThemian" target="_blank" rel="noreferrer"> 
+                            <img className = "img-thumbnail" src ={gitHubIcon} alt = 'gitHub'></img>
+                             </a>
+                        
+                        </div>
+                        </div>
+                    </div> 
                 </div>
-            </div>
-        </div>
+                </div>
+
+                        
+            
+       
+                    
+                    
+                
+            
+    
+      
       );
 }
  
